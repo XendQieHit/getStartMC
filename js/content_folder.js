7134f665eef97ac0ffd9c808224d81eeb700c064
029BLUESTAR <94945408+029BLUESTAR@users.noreply.github.com>
2024-10-11 20:59:21 +0800

    Update content_folder.js

--- js/content_folder.js
+++ js/content_folder.js
@@ -1,19 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     
     // 获取所有触发按钮
-    const triggerButtons = document.querySelectorAll('button');
+    const triggerButtons = document.querySelectorAll('.content_list_folder');
     // 为每个按钮添加点击事件监听器
     triggerButtons.forEach(button => {
         button.addEventListener('click',function(event) {
             const content = document.getElementById(event.target.getAttribute('data-target'));
-            if (content.classList.contains('open')) {
-                content.classList.remove('open');
-            } else {
-                content.classList.add('open');
-            }
+            content.classList.add('open');
         });
     });
 });
+
 function search(key){
     const list = ["游戏下载","MOD模组、整合包安装","多人联机方法","正版购买与登录","存档、资源包导入","光影安装","服务器的简单部署","皮肤、披风、玩家名更改","转移和修改玩家数据","手动部署Java环境及其注意事项","Java版与基岩版跨平台联机","迁移中国版存档的注意事项"]
     let temp = '';
@@ -25,7 +22,9 @@ function search(key){
     if (key!=undefined){
         temp += '<div><a href="">正在建设中...</a></div>';
         document.getElementById('search_result').innerHTML = temp;
+        document.getElementById('search_result').style.maxHeight = '500vh';
     } else {
         document.getElementById('search_result').innerHTML = '';
+        document.getElementById('search_result').style.maxHeight = '0vh';
     }
 }
